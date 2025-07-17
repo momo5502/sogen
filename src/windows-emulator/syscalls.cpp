678cf78bd8c13becddc175bc9f2169807c28ca27
Hacksign <evilsign@gmail.com>
2025-07-17 15:06:53 +0800

    remove duplicated include statement

--- src/windows-emulator/syscalls.cpp
+++ src/windows-emulator/syscalls.cpp
@@ -1,4 +1,3 @@
-#include "platform/status.hpp"
 #include "std_include.hpp"
 #include "syscall_dispatcher.hpp"
 #include "cpu_context.hpp"
@@ -379,10 +378,10 @@ namespace syscalls
     NTSTATUS handle_NtCancelTimer();
 
     // syscalls/window.cpp:
-    NTSTATUS handle_NtUserBuildHwndList(const syscall_context& c, const handle desktop_handle,
-                                            const handle parent_handle,
-                                            const BOOLEAN is_children, const ULONG thread_id,
-                                            const ULONG hwnd, handle hwnd_list, ULONG hwnd_needed);
+    NTSTATUS handle_NtUserBuildHwndList(const syscall_context& c, handle desktop_handle,
+                                            handle parent_handle,
+                                            BOOLEAN is_children, ULONG thread_id,
+                                            ULONG hwnd, handle hwnd_list, ULONG hwnd_needed);
 
     // syscalls/token.cpp:
     NTSTATUS
