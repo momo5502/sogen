a9de3c5611322615d62dbc09f25619702e01ab6b
brian <brian763@163.com>
2025-10-01 01:41:25 +0800

    Replace all %ll format specifiers with portable PRI macros

--- src/samples/test-sample/test.cpp
+++ src/samples/test-sample/test.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cinttypes>
 #include <cstring>
 #include <string>
 #include <fstream>
@@ -798,7 +799,7 @@ namespace
     void print_time()
     {
         const auto epoch_time = std::chrono::system_clock::now().time_since_epoch();
-        printf("Time: %lld\n", std::chrono::duration_cast<std::chrono::nanoseconds>(epoch_time).count());
+        printf("Time: %" PRId64 "\n", std::chrono::duration_cast<std::chrono::nanoseconds>(epoch_time).count());
     }
 
     bool test_apis()
