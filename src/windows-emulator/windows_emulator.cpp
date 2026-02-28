92776e719db79838c00ad41391cf02f40aa28fd8
momo5502 <mauriceheumann@gmail.com>
2026-02-28 16:07:14 +0100

    Fetch first full profile sid

--- src/windows-emulator/windows_emulator.cpp
+++ src/windows-emulator/windows_emulator.cpp
@@ -338,9 +338,9 @@ namespace
 
     std::string get_user_sid_string(registry_manager& registry)
     {
-        constexpr auto profile_list_path = R"(\Registry\Machine\Software\Microsoft\Windows NT\CurrentVersion\ProfileList)";
+        const std::filesystem::path profile_list_path = R"(\Registry\Machine\Software\Microsoft\Windows NT\CurrentVersion\ProfileList)";
 
-        const auto profile_list_key = registry.get_key({profile_list_path});
+        const auto profile_list_key = registry.get_key(profile_list_path);
         if (!profile_list_key)
         {
             throw std::runtime_error("Failed to get ProfileList registry key");
@@ -354,7 +354,14 @@ namespace
                 break;
             }
 
-            if (value->starts_with("S-1-5-21-"))
+            const auto profile_key = registry.get_key(profile_list_path / *value);
+            if (!profile_key.has_value())
+            {
+                continue;
+            }
+
+            const auto full_profile = registry.get_value(*profile_key, "FullProfile");
+            if (full_profile && full_profile->as_dword().value_or(0) == 1)
             {
                 return std::string(*value);
             }
