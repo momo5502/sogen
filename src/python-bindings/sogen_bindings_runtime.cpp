e741c6edd40083d700819b8f2e83a5a56636cb62
momo5502 <mauriceheumann@gmail.com>
2026-05-12 10:59:06 +0200

    Split Python bindings into smaller translation units

---
+++ src/python-bindings/sogen_bindings_runtime.cpp
@@ -0,0 +1,191 @@
+#include <nanobind/nanobind.h>
+
+#include "sogen_bindings_common.hpp"
+
+void register_sogen_runtime_bindings(nb::module_& m)
+{
+    nb::class_<hook_handle>(m, "Hook").def("remove", &hook_handle::remove).def_prop_ro("active", &hook_handle::active);
+
+    nb::class_<hook_registry>(m, "Hooks")
+        .def("memory_execution", &hook_registry::memory_execution)
+        .def("memory_execution_at", &hook_registry::memory_execution_at)
+        .def("memory_read", &hook_registry::memory_read)
+        .def("memory_write", &hook_registry::memory_write)
+        .def("instruction", &hook_registry::instruction)
+        .def("interrupt", &hook_registry::interrupt)
+        .def("memory_violation", &hook_registry::memory_violation)
+        .def("basic_block", &hook_registry::basic_block);
+
+    nb::class_<memory_manager>(m, "MemoryManager")
+        .def("read_memory",
+             [](const memory_manager& self, uint64_t address, size_t size) { return read_memory_bytes(self, address, size); })
+        .def("write_memory",
+             [](memory_manager& self, uint64_t address, const nb::bytes& buffer) { write_memory_bytes(self, address, buffer); })
+        .def(
+            "allocate_memory",
+            [](memory_manager& self, size_t size, memory_permission permissions, bool reserve_only, uint64_t start,
+               memory_region_kind kind) { return self.allocate_memory(size, permissions, reserve_only, start, kind); },
+            nb::arg("size"), nb::arg("permissions"), nb::arg("reserve_only") = false, nb::arg("start") = 0,
+            nb::arg("kind") = memory_region_kind::private_allocation)
+        .def("protect_memory",
+             [](memory_manager& self, uint64_t address, size_t size, memory_permission permissions) {
+                 return self.protect_memory(address, size, nt_memory_permission{permissions});
+             })
+        .def("commit_memory",
+             [](memory_manager& self, uint64_t address, size_t size, memory_permission permissions) {
+                 return self.commit_memory(address, size, nt_memory_permission{permissions});
+             })
+        .def("decommit_memory", &memory_manager::decommit_memory)
+        .def("release_memory", &memory_manager::release_memory)
+        .def("find_free_allocation_base",
+             static_cast<uint64_t (memory_manager::*)(size_t, uint64_t) const>(&memory_manager::find_free_allocation_base))
+        .def("get_region_info", &memory_manager::get_region_info)
+        .def("compute_memory_stats", &memory_manager::compute_memory_stats)
+        .def_prop_rw("default_allocation_address", &memory_manager::get_default_allocation_address,
+                     &memory_manager::set_default_allocation_address);
+
+    nb::class_<emulator_thread>(m, "Thread")
+        .def_prop_ro("id", [](const emulator_thread& self) { return self.id; })
+        .def_prop_ro("name", [](const emulator_thread& self) { return u16_to_u8(self.name); })
+        .def_prop_ro("start_address", [](const emulator_thread& self) { return self.start_address; })
+        .def_prop_ro("argument", [](const emulator_thread& self) { return self.argument; })
+        .def_prop_ro("executed_instructions", [](const emulator_thread& self) { return self.executed_instructions; })
+        .def_prop_ro("current_ip", [](const emulator_thread& self) { return self.current_ip; })
+        .def_prop_ro("previous_ip", [](const emulator_thread& self) { return self.previous_ip; })
+        .def_prop_ro("setup_done", [](const emulator_thread& self) { return self.setup_done; })
+        .def_prop_ro("exit_status", [](const emulator_thread& self) -> nb::object {
+            if (!self.exit_status.has_value())
+            {
+                return nb::none();
+            }
+
+            return nb::int_(*self.exit_status);
+        });
+
+    nb::class_<callback_registry>(m, "Callbacks")
+        .def("set", [](callback_registry& self, const std::string& name, nb::object callback) { self.set(name, std::move(callback)); })
+        .def("clear", [](callback_registry& self, const std::string& name) { self.clear(name); })
+        .def_prop_rw(
+            "on_module_load", [](callback_registry& self) { return self.module_load_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("module_load", std::move(callback)); })
+        .def_prop_rw(
+            "on_module_unload", [](callback_registry& self) { return self.module_unload_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("module_unload", std::move(callback)); })
+        .def_prop_rw(
+            "on_stdout", [](callback_registry& self) { return self.stdout_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("stdout", std::move(callback)); })
+        .def_prop_rw(
+            "on_syscall", [](callback_registry& self) { return self.syscall_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("syscall", std::move(callback)); })
+        .def_prop_rw(
+            "on_generic_access", [](callback_registry& self) { return self.generic_access_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("generic_access", std::move(callback)); })
+        .def_prop_rw(
+            "on_generic_activity", [](callback_registry& self) { return self.generic_activity_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("generic_activity", std::move(callback)); })
+        .def_prop_rw(
+            "on_suspicious_activity", [](callback_registry& self) { return self.suspicious_activity_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("suspicious_activity", std::move(callback)); })
+        .def_prop_rw(
+            "on_exception", [](callback_registry& self) { return self.exception_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("exception", std::move(callback)); })
+        .def_prop_rw(
+            "on_instruction", [](callback_registry& self) { return self.instruction_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("instruction", std::move(callback)); })
+        .def_prop_rw(
+            "on_memory_protect", [](callback_registry& self) { return self.memory_protect_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("memory_protect", std::move(callback)); })
+        .def_prop_rw(
+            "on_memory_allocate", [](callback_registry& self) { return self.memory_allocate_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("memory_allocate", std::move(callback)); })
+        .def_prop_rw(
+            "on_memory_violate", [](callback_registry& self) { return self.memory_violate_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("memory_violate", std::move(callback)); })
+        .def_prop_rw(
+            "on_rdtsc", [](callback_registry& self) { return self.rdtsc_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("rdtsc", std::move(callback)); })
+        .def_prop_rw(
+            "on_rdtscp", [](callback_registry& self) { return self.rdtscp_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("rdtscp", std::move(callback)); })
+        .def_prop_rw(
+            "on_ioctrl", [](callback_registry& self) { return self.ioctrl_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("ioctrl", std::move(callback)); })
+        .def_prop_rw(
+            "on_debug_string", [](callback_registry& self) { return self.debug_string_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("debug_string", std::move(callback)); })
+        .def_prop_rw(
+            "on_thread_create", [](callback_registry& self) { return self.thread_create_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("thread_create", std::move(callback)); })
+        .def_prop_rw(
+            "on_thread_terminated", [](callback_registry& self) { return self.thread_terminated_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("thread_terminated", std::move(callback)); })
+        .def_prop_rw(
+            "on_thread_set_name", [](callback_registry& self) { return self.thread_set_name_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("thread_set_name", std::move(callback)); })
+        .def_prop_rw(
+            "on_thread_switch", [](callback_registry& self) { return self.thread_switch_cb; },
+            [](callback_registry& self, nb::object callback) { self.set("thread_switch", std::move(callback)); });
+
+    nb::class_<sogen_process_context>(m, "ProcessContext")
+        .def_prop_ro("is_wow64_process", [](const sogen_process_context& self) { return self.is_wow64_process(); })
+        .def_prop_ro("exit_status",
+                     [](const sogen_process_context& self) -> nb::object {
+                         if (!self.exit_status().has_value())
+                         {
+                             return nb::none();
+                         }
+
+                         return nb::int_(*self.exit_status());
+                     })
+        .def_prop_ro("live_thread_count", &sogen_process_context::live_thread_count)
+        .def_prop_ro("spawned_thread_count", &sogen_process_context::spawned_thread_count)
+        .def_prop_ro("active_thread", &sogen_process_context::active_thread, nb::rv_policy::reference_internal)
+        .def_prop_ro("callbacks", &sogen_process_context::callback_view, nb::rv_policy::reference_internal);
+
+    nb::class_<sogen_windows_emulator>(m, "WindowsEmulator")
+        .def("start", &sogen_windows_emulator::start, nb::arg("count") = 0, nb::call_guard<nb::gil_scoped_release>())
+        .def("run", &sogen_windows_emulator::run, nb::arg("count") = 0, nb::call_guard<nb::gil_scoped_release>())
+        .def("stop", &sogen_windows_emulator::stop)
+        .def("setup_process_if_necessary", &sogen_windows_emulator::setup_process_if_necessary, nb::call_guard<nb::gil_scoped_release>())
+        .def("yield_thread", &sogen_windows_emulator::yield_thread, nb::arg("alertable") = false, nb::call_guard<nb::gil_scoped_release>())
+        .def("perform_thread_switch", &sogen_windows_emulator::perform_thread_switch, nb::call_guard<nb::gil_scoped_release>())
+        .def("activate_thread", &sogen_windows_emulator::activate_thread)
+        .def_prop_ro("executed_instructions", [](const sogen_windows_emulator& self) { return self.native().get_executed_instructions(); })
+        .def_prop_ro("last_stop_reason",
+                     [](const sogen_windows_emulator& self) { return stop_reason_to_string(self.native().last_stop_reason()); })
+        .def_prop_ro("last_stop_reason_code",
+                     [](const sogen_windows_emulator& self) { return static_cast<int>(self.native().last_stop_reason()); })
+        .def_prop_ro("last_stop_detail", [](const sogen_windows_emulator& self) { return self.native().last_stop_detail(); })
+        .def_prop_ro("backend_name", [](const sogen_windows_emulator& self) { return self.native().emu().get_name(); })
+        .def_prop_ro("emulation_root", [](const sogen_windows_emulator& self) { return self.native().emulation_root; })
+        .def_prop_ro("process", &sogen_windows_emulator::process, nb::rv_policy::reference_internal)
+        .def_prop_ro("memory", &sogen_windows_emulator::memory, nb::rv_policy::reference_internal)
+        .def_prop_ro("current_thread", &sogen_windows_emulator::current_thread, nb::rv_policy::reference_internal)
+        .def_prop_ro("current_thread_id",
+                     [](const sogen_windows_emulator& self) -> nb::object {
+                         const auto id = self.current_thread_id();
+                         if (!id.has_value())
+                         {
+                             return nb::none();
+                         }
+
+                         return nb::int_(*id);
+                     })
+        .def_prop_ro(
+            "callbacks", [](sogen_windows_emulator& self) -> callback_registry& { return *self.callbacks; },
+            nb::rv_policy::reference_internal)
+        .def_prop_ro(
+            "hooks", [](sogen_windows_emulator& self) -> hook_registry& { return *self.hooks; }, nb::rv_policy::reference_internal)
+        .def("read_memory", &sogen_windows_emulator::read_memory)
+        .def("write_memory", &sogen_windows_emulator::write_memory)
+        .def("read_register", &sogen_windows_emulator::read_register)
+        .def("write_register", &sogen_windows_emulator::write_register)
+        .def("get_host_port", &sogen_windows_emulator::get_host_port)
+        .def("get_emulator_port", &sogen_windows_emulator::get_emulator_port)
+        .def("map_port", &sogen_windows_emulator::map_port);
+
+    m.def("create_empty", [](nb::kwargs kwargs) { return sogen_windows_emulator(create_empty_emulator(kwargs)); });
+    m.def("create_application", [](nb::object application, nb::object args, nb::kwargs kwargs) {
+        return sogen_windows_emulator(create_application_emulator(application, args, kwargs));
+    });
+}
---
+++ src/python-bindings/sogen_bindings_types.cpp
@@ -0,0 +1,149 @@
+#include <nanobind/nanobind.h>
+
+#include "sogen_bindings_common.hpp"
+
+void register_sogen_types_bindings(nb::module_& m)
+{
+    nb::enum_<backend_type>(m, "Backend")
+        .value("auto_select", backend_type::auto_select)
+        .value("unicorn", backend_type::unicorn)
+        .value("icicle", backend_type::icicle)
+        .value("whp", backend_type::whp)
+        .export_values();
+
+    nb::enum_<memory_permission>(m, "MemoryPermission")
+        .value("none", memory_permission::none)
+        .value("read", memory_permission::read)
+        .value("write", memory_permission::write)
+        .value("exec", memory_permission::exec)
+        .value("read_write", memory_permission::read_write)
+        .value("all", memory_permission::all)
+        .export_values();
+
+    nb::enum_<memory_region_kind>(m, "MemoryRegionKind")
+        .value("free", memory_region_kind::free)
+        .value("private_allocation", memory_region_kind::private_allocation)
+        .value("file_section_view", memory_region_kind::file_section_view)
+        .value("pagefile_section_view", memory_region_kind::pagefile_section_view)
+        .value("section_image", memory_region_kind::section_image)
+        .value("mmio", memory_region_kind::mmio)
+        .export_values();
+
+    nb::enum_<memory_violation_type>(m, "MemoryViolationType")
+        .value("unmapped", memory_violation_type::unmapped)
+        .value("protection", memory_violation_type::protection)
+        .export_values();
+
+    nb::enum_<x86_register>(m, "Register")
+        .value("invalid", x86_register::invalid)
+        .value("rax", x86_register::rax)
+        .value("rbx", x86_register::rbx)
+        .value("rcx", x86_register::rcx)
+        .value("rdx", x86_register::rdx)
+        .value("rsi", x86_register::rsi)
+        .value("rdi", x86_register::rdi)
+        .value("rbp", x86_register::rbp)
+        .value("rsp", x86_register::rsp)
+        .value("rip", x86_register::rip)
+        .value("r8", x86_register::r8)
+        .value("r9", x86_register::r9)
+        .value("r10", x86_register::r10)
+        .value("r11", x86_register::r11)
+        .value("r12", x86_register::r12)
+        .value("r13", x86_register::r13)
+        .value("r14", x86_register::r14)
+        .value("r15", x86_register::r15)
+        .value("eax", x86_register::eax)
+        .value("ebx", x86_register::ebx)
+        .value("ecx", x86_register::ecx)
+        .value("edx", x86_register::edx)
+        .value("esi", x86_register::esi)
+        .value("edi", x86_register::edi)
+        .value("ebp", x86_register::ebp)
+        .value("esp", x86_register::esp)
+        .value("eip", x86_register::eip)
+        .value("eflags", x86_register::eflags)
+        .value("rflags", x86_register::rflags)
+        .value("cs", x86_register::cs)
+        .value("ss", x86_register::ss)
+        .value("ds", x86_register::ds)
+        .value("es", x86_register::es)
+        .value("fs", x86_register::fs)
+        .value("gs", x86_register::gs)
+        .value("xmm0", x86_register::xmm0)
+        .value("xmm1", x86_register::xmm1)
+        .value("xmm2", x86_register::xmm2)
+        .value("xmm3", x86_register::xmm3)
+        .value("xmm4", x86_register::xmm4)
+        .value("xmm5", x86_register::xmm5)
+        .value("xmm6", x86_register::xmm6)
+        .value("xmm7", x86_register::xmm7)
+        .value("xmm8", x86_register::xmm8)
+        .value("xmm9", x86_register::xmm9)
+        .value("xmm10", x86_register::xmm10)
+        .value("xmm11", x86_register::xmm11)
+        .value("xmm12", x86_register::xmm12)
+        .value("xmm13", x86_register::xmm13)
+        .value("xmm14", x86_register::xmm14)
+        .value("xmm15", x86_register::xmm15)
+        .export_values();
+
+    nb::class_<memory_stats>(m, "MemoryStats")
+        .def_prop_ro("reserved_memory", [](const memory_stats& self) { return self.reserved_memory; })
+        .def_prop_ro("committed_memory", [](const memory_stats& self) { return self.committed_memory; });
+
+    nb::class_<handle>(m, "Handle")
+        .def(nb::init<>())
+        .def_prop_rw(
+            "bits", [](const handle& self) { return self.bits; }, [](handle& self, uint64_t value) { self.bits = value; })
+        .def_prop_ro("id", [](const handle& self) { return self.value.id; })
+        .def_prop_ro("type", [](const handle& self) { return self.value.type; })
+        .def_prop_ro("is_system", [](const handle& self) { return self.value.is_system != 0; })
+        .def_prop_ro("is_pseudo", [](const handle& self) { return self.value.is_pseudo != 0; })
+        .def_prop_ro("high_bits", [](const handle& self) { return self.value.high_bits; });
+
+    nb::class_<region_info>(m, "MemoryRegionInfo")
+        .def_prop_ro("start", [](const region_info& self) { return self.start; })
+        .def_prop_ro("length", [](const region_info& self) { return self.length; })
+        .def_prop_ro("permissions", [](const region_info& self) { return self.permissions; })
+        .def_prop_ro("allocation_base", [](const region_info& self) { return self.allocation_base; })
+        .def_prop_ro("allocation_length", [](const region_info& self) { return self.allocation_length; })
+        .def_prop_ro("is_reserved", [](const region_info& self) { return self.is_reserved; })
+        .def_prop_ro("is_committed", [](const region_info& self) { return self.is_committed; })
+        .def_prop_ro("initial_permissions", [](const region_info& self) { return self.initial_permissions; })
+        .def_prop_ro("kind", [](const region_info& self) { return self.kind; });
+
+    nb::enum_<instruction_hook_continuation>(m, "HookContinuation")
+        .value("run", instruction_hook_continuation::run_instruction)
+        .value("skip", instruction_hook_continuation::skip_instruction)
+        .export_values();
+
+    nb::enum_<memory_violation_continuation>(m, "MemoryViolationContinuation")
+        .value("stop", memory_violation_continuation::stop)
+        .value("resume", memory_violation_continuation::resume)
+        .value("restart", memory_violation_continuation::restart)
+        .export_values();
+
+    nb::enum_<x86_hookable_instructions>(m, "Instruction")
+        .value("invalid", x86_hookable_instructions::invalid)
+        .value("syscall", x86_hookable_instructions::syscall)
+        .value("cpuid", x86_hookable_instructions::cpuid)
+        .value("rdtsc", x86_hookable_instructions::rdtsc)
+        .value("rdtscp", x86_hookable_instructions::rdtscp)
+        .export_values();
+
+    nb::class_<basic_block>(m, "BasicBlock")
+        .def_prop_ro("address", [](const basic_block& self) { return self.address; })
+        .def_prop_ro("instruction_count", [](const basic_block& self) { return self.instruction_count; })
+        .def_prop_ro("size", [](const basic_block& self) { return self.size; });
+
+    nb::class_<mapped_module>(m, "MappedModule")
+        .def_prop_ro("name", [](const mapped_module& self) { return self.name; })
+        .def_prop_ro("path", [](const mapped_module& self) { return self.path; })
+        .def_prop_ro("module_path", [](const mapped_module& self) { return self.module_path.string(); })
+        .def_prop_ro("image_base", [](const mapped_module& self) { return self.image_base; })
+        .def_prop_ro("image_base_file", [](const mapped_module& self) { return self.image_base_file; })
+        .def_prop_ro("size_of_image", [](const mapped_module& self) { return self.size_of_image; })
+        .def_prop_ro("entry_point", [](const mapped_module& self) { return self.entry_point; })
+        .def_prop_ro("is_static", [](const mapped_module& self) { return self.is_static; });
+}
