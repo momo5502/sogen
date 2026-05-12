35ad5aca910deb61f99571917d70594006f6be27
momo5502 <mauriceheumann@gmail.com>
2026-05-12 13:32:40 +0200

    Add CMake flag to disable nanobind leak warnings

--- src/python-bindings/sogen_module.cpp
+++ src/python-bindings/sogen_module.cpp
@@ -6,5 +6,8 @@ void register_sogen_bindings(nb::module_& m);
 
 NB_MODULE(sogen, m)
 {
+#ifdef SOGEN_DISABLE_NANOBIND_LEAK_WARNINGS
+    nb::set_leak_warnings(false);
+#endif
     register_sogen_bindings(m);
 }
