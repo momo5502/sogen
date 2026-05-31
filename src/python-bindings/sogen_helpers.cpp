512da00f11abaa6a31a4835e94f7fda6a70d8acf
momo5502 <mauriceheumann@gmail.com>
2026-05-31 17:31:56 +0200

    Handle deadlock stop reason

--- src/python-bindings/sogen_helpers.cpp
+++ src/python-bindings/sogen_helpers.cpp
@@ -14,6 +14,8 @@ namespace sogen::py
             return "unimplemented_syscall";
         case stop_reason::syscall_exception:
             return "syscall_exception";
+        case stop_reason::deadlock:
+            return "deadlock";
         }
 
         return "unknown";
