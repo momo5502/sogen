abc4947f96947d407f4680e48cc24dea4ef98040
momo5502 <mauriceheumann@gmail.com>
2026-06-01 20:18:35 +0200

    Simplify web UI backend ifdefs

--- src/windows-emulator/ui_backends/web_ui_backend.cpp
+++ src/windows-emulator/ui_backends/web_ui_backend.cpp
@@ -3,13 +3,11 @@
 
 #ifdef OS_EMSCRIPTEN
 #include <emscripten.h>
-#endif
 
 namespace sogen
 {
     namespace
     {
-#ifdef OS_EMSCRIPTEN
         struct queued_web_ui_event
         {
             ui_event event{};
@@ -172,21 +170,13 @@ namespace sogen
             std::scoped_lock lock{g_web_ui_event_mutex};
             g_web_ui_events.push_back(queued_web_ui_event{.event = event});
         }
-#else
-        void enqueue_web_ui_event(const ui_event& event)
-        {
-            (void)event;
-        }
-#endif
 
         class web_ui_backend final : public ui_backend
         {
           public:
             web_ui_backend()
             {
-#ifdef OS_EMSCRIPTEN
                 initialize_web_ui_bridge();
-#endif
             }
 
             void set_event_sink(event_sink sink) override
@@ -196,7 +186,6 @@ namespace sogen
 
             void pump_events() override
             {
-#ifdef OS_EMSCRIPTEN
                 std::deque<queued_web_ui_event> events{};
                 {
                     std::scoped_lock lock{g_web_ui_event_mutex};
@@ -212,70 +201,40 @@ namespace sogen
                 {
                     this->sink_(event.event);
                 }
-#endif
             }
 
             void create_window(const ui_window_desc& desc) override
             {
-#ifdef OS_EMSCRIPTEN
                 post_ui_create_window(desc);
-#else
-                (void)desc;
-#endif
             }
 
             void destroy_window(const hwnd window) override
             {
-#ifdef OS_EMSCRIPTEN
                 post_ui_message("destroy_window", window);
-#else
-                (void)window;
-#endif
             }
 
             void set_window_rect(const hwnd window, const RECT& rect) override
             {
-#ifdef OS_EMSCRIPTEN
                 post_ui_rect_message("set_rect", window, rect);
-#else
-                (void)window;
-                (void)rect;
-#endif
             }
 
             void set_window_visible(const hwnd window, const bool visible) override
             {
-#ifdef OS_EMSCRIPTEN
                 post_ui_visibility_message("set_visible", window, visible);
-#else
-                (void)window;
-                (void)visible;
-#endif
             }
 
             void set_window_enabled(const hwnd window, const bool enabled) override
             {
-#ifdef OS_EMSCRIPTEN
                 post_ui_visibility_message("set_enabled", window, enabled);
-#else
-                (void)window;
-                (void)enabled;
-#endif
             }
 
             void set_window_title(const hwnd window, const std::u16string_view title) override
             {
-#ifdef OS_EMSCRIPTEN
                 post_ui_title_message("set_title", window, title);
-#else
-                (void)window;
-                (void)title;
-#endif
             }
 
             void invalidate(const hwnd window, const std::optional<RECT>& rect) override
             {
-#ifdef OS_EMSCRIPTEN
                 if (rect)
                 {
                     post_ui_rect_message("invalidate", window, *rect);
@@ -284,20 +243,11 @@ namespace sogen
                 {
                     post_ui_message("invalidate", window);
                 }
-#else
-                (void)window;
-                (void)rect;
-#endif
             }
 
             void present_surface(const hwnd window, const ui_surface_desc& surface) override
             {
-#ifdef OS_EMSCRIPTEN
                 post_ui_present_surface(window, surface);
-#else
-                (void)window;
-                (void)surface;
-#endif
             }
 
           private:
@@ -309,15 +259,22 @@ namespace sogen
     {
         return std::make_unique<web_ui_backend>();
     }
-}
 
-#ifdef OS_EMSCRIPTEN
-namespace sogen
-{
     extern "C" EMSCRIPTEN_KEEPALIVE void sogen_web_ui_push_event(const uint32_t window, const uint32_t message, const uint32_t wparam,
                                                                  const uint32_t lparam)
     {
         enqueue_web_ui_event(ui_event{.window = window, .message = message, .wParam = wparam, .lParam = lparam});
     }
 }
+
+#else
+
+namespace sogen
+{
+    std::unique_ptr<ui_backend> create_web_ui_backend()
+    {
+        return std::make_unique<null_ui_backend>();
+    }
+}
+
 #endif
