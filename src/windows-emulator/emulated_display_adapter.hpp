f0229b4d6c4b635e6677151eaa502d51e84b3b69
xop <73129829+xop01@users.noreply.github.com>
2026-09-13 19:02:13 +0300

    fix(dxgk): drop redundant namespace-closing comment

--- src/windows-emulator/emulated_display_adapter.hpp
+++ src/windows-emulator/emulated_display_adapter.hpp
@@ -14,4 +14,4 @@ namespace sogen::emulated_display
     constexpr std::u16string_view hardware_id = u"PCI\\VEN_1414&DEV_008C&SUBSYS_00000000&REV_00";
     constexpr std::u16string_view interface_path =
         u"\\\\?\\PCI#VEN_1414&DEV_008C&SUBSYS_00000000&REV_00#4&1234567&0&0008#{5b45201d-f2f2-4f3b-85bb-30ff1f953599}";
-} // namespace sogen::emulated_display
+}
